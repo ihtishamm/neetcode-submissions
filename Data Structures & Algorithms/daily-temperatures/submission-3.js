31243265ee85920215a068f22cf3c5b4063562e7
Ihtisham Hassan <ihtishamhassan1111@gmail.com>
2026-08-20 15:22:49 +0500

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let res = new Array(temperatures.length).fill(0);
+        let stack = []; // pair of value and index
+
+        for (let i = 0; i < temperatures.length; i++) {
+            let t = temperatures[i];
+          while (stack.length > 0 && t > stack[stack.length - 1][0]) {
+                const [stackT, stackInd] = stack.pop();
+                res[stackInd] = i - stackInd;
+            }
+            stack.push([t, i]);
+        }
+        return res;
+    }
+}
